6fcadb146da853b84f2c2f0b9e2631c04ae2ae31
Thibault <156127071+Tboworst@users.noreply.github.com>
2026-05-25 13:50:36 -0400

    added hashing for specific errors amd added test to sample.jsonl to check if the hashsing works for same exact bug

--- fingerprinting.py
+++ fingerprinting.py
@@ -10,3 +10,7 @@ def fingerprint(event):
     
     #this happens once everything is looped through in the list
     function_chain = "->".join(result)
+
+    fingerprint_str = event["exception_type"]+"|"+normalize_message(event["message"])+"|"+function_chain
+    #hashing this message so that we can pair it with other messages
+    return hashlib.md5(fingerprint_str.encode()).hexdigest()
